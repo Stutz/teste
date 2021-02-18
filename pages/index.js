d17c66d45f438ceb05b6a02bf9b2a1b09c331e5f
Dalmo Stutz <dstutz.br@gmail.com>
2021-02-17 21:39:55 -0300

    sobree

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
+import Link from 'next/link'
+
 function Home(){
-    return <div>Home Teste</div>
+    return (<div>
+        <h1>Página Home 2</h1>
+        <Link href="/sobre">
+            <a>Ir para a página Sobre</a>
+        </Link>      
+    </div>)
 }
 
 export default Home
---
+++ pages/sobre.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+function Sobre(){
+    return (<div>
+        <h1>Página sobre 2</h1>
+        <Link href="/">
+            <a>Ir para a página Home</a>
+        </Link>      
+    </div>)
+}
+
+export default Sobre
